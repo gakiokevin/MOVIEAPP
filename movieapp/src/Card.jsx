38c24bf8d846bb4ec1bee49864f7ce966fc10b8b
kelvin Macharia <101465969+gakiokevin@users.noreply.github.com>
2023-09-16 23:47:29 +0300

    Update Card.jsx

--- movieapp/src/Card.jsx
+++ movieapp/src/Card.jsx
@@ -18,7 +18,7 @@ const CardImage = ({ images }) => {
           data-testid="movie-poster"
         >
           <img
-            src={"/images/favourite.png"}
+            src={"dist/favourite.png"}
             alt={"favourite"}
             className={clicked[index] ? "clicked" : " "}
           />
